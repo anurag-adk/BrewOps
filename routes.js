99b5ad1e6863f881d5f2ebea65bdc8624f21e6ee
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-10-07 00:57:21 +0545

    Define routes

---
+++ routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+//Using json file instead of DB (for demo only)
+const menu = require("./menu.json");
+
+// In-memory orders storage (for demo only)
+let orders = [];
+let orderIdCounter = 1;
+
+// Welcome...
+router.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to BrewOps",
+    description:
+      "A simple Cafe Management System developed by Anurag demonstrating concepts of DevOps, CI/CD & Containerization as an assignment for LSPP.",
+    endpoints: {
+      "GET /": "Welcome",
+      "GET /menu": "View coffee menu",
+      "POST /order": "Place a new order",
+      "GET /orders": "View all orders",
+    },
+  });
+});
+
+module.exports = router;
