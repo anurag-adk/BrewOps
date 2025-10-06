1f26303ebbb714e78ec2d72e989a4bc19b2e36c2
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-10-07 00:55:05 +0545

    Initialize server template

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const routes = require("./routes");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+
+// Routes...
+app.use("/", routes);
+
+// Undefined Routes
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Route not found",
+    message: "Please check the available endpoints at GET /",
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server started at port: ${PORT}`);
+  console.log(`BrewOps server started at port: ${PORT}`);
 });
